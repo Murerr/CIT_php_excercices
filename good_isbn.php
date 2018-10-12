1b27b40c012dbb02d45af9efcb3cca070280b155
murerr <rudy.murer.pro@gmail.com>
2018-10-12 12:30:45 +0100

    adding ISBN 10 check conditon

--- good_isbn.php
+++ good_isbn.php
@@ -4,8 +4,20 @@ function good_isbn($argv){
     $isbn_number = $argv[1]; // get argument from Linux console
     $isbn_array = str_split($isbn_number); //split each character into a array
     
-    //TODO add condition size of
+    if(sizeof($isbn_array) == 13){
+        return isbn_13($isbn_array);
+    } else if(sizeof($isbn_array) == 10){
+        return isbn_10($isbn_array);
+    } else {
+        echo "Invalid ISBN Number\n";
+        return false;
+    }    
+    
+}
+good_isbn($argv);
 
+
+function isbn_13($isbn_array){
     $res=0;
     for($i=0;$i<sizeof($isbn_array)-1;$i++){
         if($i%2 == 0){ // Multiply by 3
@@ -15,17 +27,45 @@ function good_isbn($argv){
         }
     }
     $res = $res%10;
-    $last_digit = $isbn_array[sizeof($isbn_array)-1];
+    $last_digit = (int)$isbn_array[sizeof($isbn_array)-1];
     if($res!=0){
         $check_digit = 10 - $res;
         if($check_digit == $last_digit){
             echo "succes\n";
             return true;
         }
+        else {
+            echo "False ISBN number\n";
+            return false;
+        }
+    } else {
+        echo "ERROR False ISBN number\n";
+        return false;
     }
     var_dump($res);
-    
-    
+
 }
-good_isbn($argv);
+
+function isbn_10($isbn_array){
+    $isbn_array = array_reverse($isbn_array);
+    $res = 0;
+    for($i=1;$i<sizeof($isbn_array);$i++){
+        $weigh = $i+1;
+        $res+= (int)$isbn_array[$i] * $weigh;
+        echo $res,"\n";
+    }
+    $res = 11 - $res%11;
+    $check_digit = (int)$isbn_array[0];
+    var_dump($res);
+    var_dump($check_digit);
+    if($res == $check_digit){
+        echo "succes\n";
+        return true;
+    } else {
+        echo "False ISBN number\n";
+        return false;
+    }
+
+
+}
 ?>
